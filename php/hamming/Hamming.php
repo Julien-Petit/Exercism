f2e500bedef6bccd11ac6788223f0c8691abd6c1
Julien petit <julien.petit@boondmanager.com>
2023-02-22 09:54:08 +0100

    Exo4 Hamming Excercism

---
+++ php/hamming/Hamming.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * By adding type hints and enabling strict type checking, code can become
+ * easier to read, self-documenting and reduce the number of potential bugs.
+ * By default, type declarations are non-strict, which means they will attempt
+ * to change the original type to match the type specified by the
+ * type-declaration.
+ *
+ * In other words, if you pass a string to a function requiring a float,
+ * it will attempt to convert the string value to a float.
+ *
+ * To enable strict mode, a single declare directive must be placed at the top
+ * of the file.
+ * This means that the strictness of typing is configured on a per-file basis.
+ * This directive not only affects the type declarations of parameters, but also
+ * a function's return type.
+ *
+ * For more info review the Concept on strict type checking in the PHP track
+ * <link>.
+ *
+ * To disable strict typing, comment out the directive below.
+ */
+
+declare(strict_types=1);
+
+
+/**
+ * @throws Exception
+ */
+function distance(string $strandA, string $strandB): int
+{
+
+   if (strlen($strandA) == strlen($strandB)) {
+       $distance = 0;
+
+       for ($result = 0; $result < strlen($strandA); $result++) {
+           if($strandA[$result] != $strandB[$result]) {
+               $distance++;
+           }
+       }
+
+       return $distance;
+       // boucler sur toutes les comparaison possible entre les 2 chaines de caractères
+   }
+   else {
+       throw new InvalidArgumentException('DNA strands must be of equal length.');
+       // ensuite un else pour retourner une exception...
+   }
+}
